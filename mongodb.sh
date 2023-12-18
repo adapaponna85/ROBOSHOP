be79bf68bbefbb3c11e3a79349144369ed5ac0ca
adapaponna85 <adapaponna85@gmail.com>
2023-12-18 12:02:39 -0500

    Added new mongodb.sh file

--- mongodb.sh
+++ mongodb.sh
@@ -8,10 +8,39 @@ N="\e[0m"
 Timestamp=$(date +%F-%H-%M-%S)
 Logfile="/tmp/$0-$Timestamp.log"
 
+VALIDATE()
+{
+    if [ $? -ne 0 ]
+    then
+    echo -e "$R $2 Error $N"
+    exit 1
+    else
+    echo -e "$G $2 Success $N"
+    fi
+}
+
 if [ $user -ne 0 ]
 then
 echo -e " $R Unauthorized user! Please login as root user to proceed.. $N"
 else
 echo -e " $G Authorized user! Proceeding.. $N"
 fi
+
+cp mongo.repo /etc/yum.repos.d/ &>> Logfile
+VALIDATE $? "Copying mongo.repo file"
+
+dnf install mongodb-org -y &>> Logfile
+VALIDATE $? "Installing MONGODB"
+
+systemctl enable mongod &>> Logfile
+VALDIATE $? "Enabling MONGODB"
+
+systemctl start mongod &>> Logile
+VALIDATE $? "Starting MONGODB"
+
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>> Logfile
+VALIDATE $? "Remote Access to MONGODB" 
+
+systemctl restart mongod &>> Logfile
+VALIDATE $? "Restarting MONGODB"
 
