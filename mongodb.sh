0dd2983144e09772f5b37236192f3a8ad3faef3d
adapaponna85 <adapaponna85@gmail.com>
2023-12-18 11:44:50 -0500

    Added new mongodb.sh file

---
+++ mongodb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+user=$(id -u)
+R="e\[31m"
+G="e\[32m"
+Y="e\[33m"
+N="e\[0m"
+
+Timestamp=$(date +%F-$H-$M-$S")
+Logfile="($0-$Timestamp.log)"
+
+if [ $? -ne 0 ]
+then
+echo -e "$R Unauthorized user! Please login as root user to proceed.. $N"
+else
+echo -e "$G Authorized user! Proceeding.. $N"
+fi
+
