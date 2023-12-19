70126f7ef3d9df0a7d924f43a00cac30f4a94ec6
adapaponna85 <adapaponna85@gmail.com>
2023-12-19 02:04:49 -0500

    Added files

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -4,7 +4,6 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 
-MONGODB_Host=mongodb.apdevops.online
 Timestamp=$(date +%F-%H-%M-%S)
 Logfile="/tmp/$0-$Timestamp.log"
 
@@ -27,3 +26,23 @@ VALIDATE()
     fi 
 }
 
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash   &>> $Logfile
+VALIDATE $? "Downlaoding erlang script"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash   &>> $Logfile
+VALIDATE $? "Downloading rabbitmq script"
+
+dnf install rabbitmq-server -y  &>> $Logfile
+VALIDATE $? "Installing rabbitmq server"
+
+systemctl enable rabbitmq-server   &>> $Logfile
+VALIDATE $? "Enabling rabbitmq server"
+
+systemctl start rabbitmq-server  &>> $Logfile
+VALIDATE $? "Starting rabbitmq server"
+
+rabbitmqctl add_user roboshop roboshop123  &>> $Logfile
+VALIDATE $? "Adding Roboshop user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"   &>> $Logfile
+VALIDATE $? "Setting permissions Nodejs"
