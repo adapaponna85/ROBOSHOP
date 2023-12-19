2d3c070af13592394f0b711c390b82b2beb96ae9
adapaponna85 <adapaponna85@gmail.com>
2023-12-19 15:27:25 -0500

    Added InstanceCreation.sh

---
+++ InstanceCreation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-01833f2b4eac3f880
+
+Instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${Instances[@]}"
+do
+echo "Instance is : $i"
+if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+then
+Instance_Type="t3.small"
+else
+Instance_Type="t2.micro"
+fi
+aws ec2 run-instances --image-id $AMI  --instance-type $Instance_Type  --security-group-ids $SG_ID
+
+done
